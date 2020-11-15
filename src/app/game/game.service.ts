abeab8bb55b1a34a7b6732592234ef5cdc632fef
Guy Krief <guy.krief6@gmail.com>
2020-11-15 13:03:43 +0200

    removed unused imports

--- src/app/game/game.service.ts
+++ src/app/game/game.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Observable, of} from 'rxjs';
-import {catchError, map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 import {Question} from '../core/models/question';
 import {Store} from '@ngrx/store';
 import {CORRECT_HEADER, INCORRECT_HEADER, QUESTION_HEADER} from '../core/constants/global';
